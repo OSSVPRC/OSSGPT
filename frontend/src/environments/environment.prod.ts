5aa049376aee922b9b40cd2ed2e84a70417fb555
hamdafoughali-gif <hamda.foughali@gmail.com>
2026-07-07 17:43:59 +0100

    MAJ .env.example et environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // Remplacez NOM-BACKEND par l'URL réelle de votre backend sur Render
+  // Remplacez par l'URL de votre backend déployé sur Render
   // Exemple : 'https://ossgpt-backend.onrender.com/api'
-  apiUrl: 'https://NOM-BACKEND.onrender.com/api',
+  apiUrl: 'https://ossgpt-backend.onrender.com/api',
 };
